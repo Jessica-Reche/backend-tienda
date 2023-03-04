7b8b52c985e51e4fe6ec11d141b222996dbb0f5e
Jessica <jessikareix20@gmail.com>
2023-03-04 19:35:09 +0100

    cambios hechos

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const helmet = require('helmet');
 const path = require('path');
 const morgan = require('morgan');
 require('./config/config');
-require('../middlewares/authmiddleware'); 
+require('./middlewares/authmiddleware');
 const app = express();
 
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const app = require('./app');
+const app = require('./app')
+ require('./middlewares/authmiddleware');
 require('dotenv').config();
 require('./database');
 
