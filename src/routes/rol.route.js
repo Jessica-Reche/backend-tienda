5b1d59cbb8b6705ba378e850e750c4974bc834cd
Jessica <jessikareix20@gmail.com>
2023-03-04 14:46:15 +0100

    cambios

--- src/routes/rol.route.js
+++ src/routes/rol.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getRol,getRols,createRol,updateRol,deleteRol} 
 = require('../controllers/rol.controller');
-const auth = require("../middlewares/authMiddleware");
+const auth = require("../middlewares/authMiddleware"); 
 
 
 //Routes for the products
